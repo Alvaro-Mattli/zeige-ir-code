9afcee98771c831669f0f8517ba3c7a9dab4ee92
Alvaro-Mattli <74493375+Alvaro-Mattli@users.noreply.github.com>
2023-06-27 19:04:11 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
 IR.IR_callbackUser(function (message) {
-    basic.showNumber(IR.IR_read())
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    if (message == 11) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    }
+    if (message == 7) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 27)
+    }
+    if (message == 8) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 27)
+    }
+    if (message == 4) {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
+    if (message == 13) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    }
 })
